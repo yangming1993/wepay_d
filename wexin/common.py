3367dfad137c502395d4e07c5459c97f85dc6203
ym <403666612@qq.com>
2017-09-12 15:52:38 +0800

    修改方法为类 增加通用请求类

--- wexin/common.py
+++ wexin/common.py
@@ -12,48 +12,103 @@ from config import WxPayConf_pub
 import hashlib
 import xml.etree.ElementTree as ET
 
-def createRdStr(length=32):
-    """产生随机字符串，不长于32位"""
-    chars = "abcdefghijklmnopqrstuvwxyz0123456789"
-    strs = []
-    for x in range(length):
-        strs.append(chars[random.randrange(0, len(chars))])
-    return "".join(strs)
 
-def formatBizQueryParaMap(paraMap, urlEncode):
-    """格式化参数，签名过程需要使用"""
-    slist = sorted(paraMap)
-    buff = []
-    for k in slist:
-        v = quote(paraMap[k]) if urlEncode else paraMap[k]
-        buff.append("{0}={1}".format(k, v))
+class Common_util_pub(object):
+    """所有接口的基类"""
 
-    return "&".join(buff)
+    @classmethod
+    def trimString(self, value):
+        '''按照微信规则，不为空值不传输'''
+        if value is not None and len(value) == 0:
+            value = None
+        return value
 
-def getSign(obj):
-    """生成签名"""
-    #签名步骤一：按字典序排序参数,formatBizQueryParaMap已做
-    String = formatBizQueryParaMap(obj, False)
-    #签名步骤二：在string后加入KEY
-    String = "{0}&key={1}".format(String,WxPayConf_pub.KEY)
-    #签名步骤三：MD5加密
-    String = hashlib.md5(String).hexdigest()
-    #签名步骤四：所有字符转为大写
-    result_ = String.upper()
+    @classmethod
+    def createRdStr(length=32):
+        """产生随机字符串，不长于32位"""
+        chars = "abcdefghijklmnopqrstuvwxyz0123456789"
+        strs = []
+        for x in range(length):
+            strs.append(chars[random.randrange(0, len(chars))])
+        return "".join(strs)
 
-    return result_
+    @classmethod
+    def formatBizQueryParaMap(paraMap, urlEncode):
+        """格式化参数，签名过程需要使用"""
+        slist = sorted(paraMap)
+        buff = []
+        for k in slist:
+            v = quote(paraMap[k]) if urlEncode else paraMap[k]
+            buff.append("{0}={1}".format(k, v))
 
-def arrayToXml(arr):
-    """array转xml"""
-    xml = ["<xml>"]
-    for k, v in arr.iteritems():
-        if v.isdigit():
-            xml.append("<{0}>{1}</{0}>".format(k, v))
-        else:
-            xml.append("<{0}><![CDATA[{1}]]></{0}>".format(k, v))
-    xml.append("</xml>")
-    return "".join(xml)
+        return "&".join(buff)
 
-def xmlToArray(xml):
-    """将xml转为array"""
-    return dict((child.tag, child.text) for child in ET.fromstring(xml))
+    def getSign(self, obj):
+        """生成签名"""
+        #签名步骤一：按字典序排序参数,formatBizQueryParaMap已做
+        String = self.formatBizQueryParaMap(obj, False)
+        #签名步骤二：在string后加入KEY
+        String = "{0}&key={1}".format(String,WxPayConf_pub.KEY)
+        #签名步骤三：MD5加密
+        String = hashlib.md5(String).hexdigest()
+        #签名步骤四：所有字符转为大写
+        result_ = String.upper()
+
+        return result_
+
+    @classmethod
+    def arrayToXml(arr):
+        """array转xml"""
+        xml = ["<xml>"]
+        for k, v in arr.iteritems():
+            if v.isdigit():
+                xml.append("<{0}>{1}</{0}>".format(k, v))
+            else:
+                xml.append("<{0}><![CDATA[{1}]]></{0}>".format(k, v))
+        xml.append("</xml>")
+        return "".join(xml)
+
+    @classmethod
+    def xmlToArray(xml):
+        """将xml转为array"""
+        return dict((child.tag, child.text) for child in ET.fromstring(xml))
+    #TODO: 将xml以post方式发送
+
+
+class Wxpay_client_pub(Common_util_pub):
+    """请求型接口的基类"""
+    response = None  #微信返回的响应
+    url = None       #接口链接
+    curl_timeout = None #curl超时时间
+
+    def __init__(self):
+        self.parameters = {} #请求参数，类型为关联数组
+        self.result = {}     #返回参数，类型为关联数组
+
+
+    def setParameter(self, parameter, parameterValue):
+        """设置请求参数"""
+        self.parameters[self.trimString(parameter)] = self.trimString(parameterValue)
+
+    def createXml(self):
+        """设置标配的请求参数，生成签名，生成接口参数xml"""
+        self.parameters["appid"] = WxPayConf_pub.APPID   #公众账号ID
+        self.parameters["mch_id"] = WxPayConf_pub.MCHID   #商户号
+        self.parameters["nonce_str"] = self.createRdStr()   #随机字符串
+        self.parameters["sign"] = self.getSign(self.parameters)   #签名
+        return  self.arrayToXml(self.parameters)
+
+    def postXml(self):
+        """post请求xml"""
+        xml = self.createXml()
+        self.response = self.postXmlCurl(xml, self.url, self.curl_timeout)
+        return self.response
+
+
+    def getResult(self):
+        """获取结果，默认不使用证书"""
+        self.postXml()
+        self.result = self.xmlToArray(self.response)
+        return self.result
+
+
---
+++ wexin/unipay.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#encoding: utf-8
+'''
+@author:ym
+
+@time:2017/9/12
+'''
+
+$END$
