0dcefa9e053bdd1e8add8c2de5767b976663a639
ym <403666612@qq.com>
2017-09-13 11:29:25 +0800

    完成native支付

--- wepay/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.pay),
-    url(r'^commit/$', views.commit)
-]
--- wepay/wexin/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-#encoding: utf-8
-'''
-@author:ym
-
-@time:2017/9/12
-'''
-
-$END$
